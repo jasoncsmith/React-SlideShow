9e7df507cc31c1ca6ef2eda08f8a960c74478e6b
Jason C Smith <smithones@gmail.com>
2024-01-10 17:32:02 -0700

    out with the old in with the new

--- src/components/slider/slider.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../../App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const el = screen.getByText(/A small custom code demo written in React 18 using functional components/i);
-  expect(el).toBeInTheDocument();
-});
